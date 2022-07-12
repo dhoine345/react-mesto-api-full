b99760820838f14f7d6812270f4ba9ea89eed82b
dhoine345 <lue42@yandex.ru>
2022-07-12 13:59:25 +0300

    some changes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { regexUrl } = require('./utils/constants');
 const NotFoundError = require('./utils/errors/NotFoundError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+//const { requestLogger, errorLogger } = require('./middlewares/logger');
 //const cors = require('./middlewares/cors')
 
 const { PORT = 3000 } = process.env;
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(requestLogger);
+//app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -81,7 +81,7 @@ app.use(() => {
   throw new NotFoundError(errorMessages.pageNotFound);
 });
 
-app.use(errorLogger);
+//app.use(errorLogger);
 
 app.use(errors());
 
